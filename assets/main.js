e260c77dcea9b57fb2e1a4aaec9840d498f679ed
websiddu <me@websiddu.com>
2016-12-10 21:55:11 +0000

    Updating to websiddu/styleguide.websiddu.com@9425bf9f4c682a6fec994f9b8503fc4a63ae5d60.

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,87 @@
-!function(){window.browser_supports_webp=!1,"undefined"==typeof window.supported_wbp||""===window.supported_wbp?window.photoset_supports_webp=!1:window.photoset_supports_webp=!!window.supported_wbp,WebP=new Image,WebP.onload=WebP.onerror=function(){return 2!==WebP.height?window.browser_supports_webp=!1:window.browser_supports_webp=!0},WebP.src="data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";var e=function(){$(".photo-set").each(function(e,t){var o=$(t).find(".picture-wrap"),i=$(this).width(),p=[],w=0;o.each(function(e,t){p.push($(t).data("ratio")),w+=10});var n=Math.min.apply(Math,p),a=0;o.each(function(e){a+=p[e]/n}),o.each(function(e,t){var o,r=(i-w)/a,t=$(this).find("img"),s=Math.ceil(r/n)*p[e],d=Math.ceil(r/n),h=t.attr("data-src"),c=1600;s>1200?c=1600:s>599&&s<1e3?c=1200:s>300&&s<599?c=800:s<600&&s>300?c=800:s<400&&(c=500),o=h.replace(/w_auto/,"c_scale,w_"+c),$(document).width()>765?(t.height(d).width(s),$(this).css({"flex-basis":s+"px"}),$(this).find("picture").height(d)):t.height("auto").width("100%"),browser_supports_webp&&(o=o.replace(/jpg/,"webp")),t.attr("data-original",o)})})},t=function(){$("img").lazyload({effect:"fadeIn"})},o=function(){$(window).on("resize",e)};o(),e(),t()}();
+(function () {
+
+    window.browser_supports_webp = !1;
+    if ("undefined" == typeof window.supported_wbp || "" === window.supported_wbp) {
+      window.photoset_supports_webp = !1;
+    } else {
+      window.photoset_supports_webp = !!window.supported_wbp
+    }
+
+
+    WebP = new Image;
+    WebP.onload = WebP.onerror = function() {
+      return 2 !== WebP.height ? window.browser_supports_webp = !1 : window.browser_supports_webp = !0
+    }
+
+    WebP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+
+
+  var _arrangeGrid = function () {
+    $('.photo-set').each(function (index, set) {
+      var images = $(set).find('.picture-wrap');
+      var photoSetWidth = $(this).width();
+      var ratios = [], margins = 0;
+      images.each(function (index, obj) {
+        ratios.push($(obj).data('ratio'));
+        margins += 10;
+      })
+      var minRatio = Math.min.apply(Math, ratios);
+      var j = 0;
+
+      images.each(function (index, obj) {
+        j += ratios[index] / minRatio
+      });
+
+      images.each(function (index, img) {
+        var c = (photoSetWidth - margins) / j,
+          img = $(this).find("img"),
+          width = Math.ceil(c / minRatio) * ratios[index],
+          height = Math.ceil(c / minRatio),
+          imgSrc = img.attr('data-src'), requestedWidth = 1600, responsiveSrc;
+
+        if (width > 1200) {
+          requestedWidth = 1600
+        } else if (width > 599 && width < 1000) {
+          requestedWidth = 1200
+        } else if (width > 300 && width < 599) {
+          requestedWidth = 800
+        } else if (width < 600 && width > 300) {
+          requestedWidth = 800
+        } else if (width < 400) {
+          requestedWidth = 500
+        }
+
+        responsiveSrc = imgSrc.replace(/w_auto/, "c_scale,w_" + requestedWidth);
+
+        if ($(document).width() > 765) {
+          img.height(height).width(width);
+          $(this).css({ 'flex-basis': width + 'px' });
+          $(this).find('picture').height(height)
+        } else {
+          img.height('auto').width('100%');
+        }
+
+        if (browser_supports_webp) {
+          responsiveSrc = responsiveSrc.replace(/jpg/, 'webp');
+        }
+
+        img.attr('data-original', responsiveSrc);
+      });
+    });
+  }
+
+  var _unveil = function () {
+    $("img").lazyload({
+      effect : "fadeIn"
+    });
+  }
+
+  var _resize = function () {
+    $(window).on('resize', _arrangeGrid);
+  }
+
+  _resize();
+  _arrangeGrid();
+  _unveil();
+
+})();
